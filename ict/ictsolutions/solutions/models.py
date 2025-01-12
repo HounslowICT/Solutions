1e1167f25491243838824c1638e4bb31a4a1ebbe
A-Jorge SamaniegoTarco <jorge.samaniegotarco@hounslow.gov.uk>
2025-01-12 17:51:41 +0000

    Fist full version commit from Work

--- ict/ictsolutions/solutions/models.py
+++ ict/ictsolutions/solutions/models.py
@@ -5,7 +5,7 @@ class Solution(models.Model):
     solution = models.TextField(max_length=1500)
     clase = models.CharField(max_length=60)
     date = models.DateTimeField(auto_now_add = True)
-    link = models.CharField(max_length=100)
+    link = models.CharField(max_length=500)
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
     
