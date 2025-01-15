e3372b2adee9cf660ade9995dc8b4b4f6d2c0f75
JorgeS <beakman23@hotmail.com>
2025-01-15 00:12:28 +0000

    Fixing the Port

--- ict/ictsolutions/ictsolutions/wsgi.py
+++ ict/ictsolutions/ictsolutions/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
 
+import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ictsolutions.settings')
 
 application = get_wsgi_application()
+
+# Set the port environment variable
+port = int(os.environ.get('PORT', 8000))
